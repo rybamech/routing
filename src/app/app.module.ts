d9d6f1eb7e6655bfd690241a3628fc7fb0e3583d
DimaAdmin <ryba-mech@yandex.ru>
2018-05-15 14:47:34 +0300

    01- add in app.module routes as const: Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,29 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  // basic routes
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contactus', redirectTo: 'contact' },
+
+  // authentication demo
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ LoggedInGuard ]
+  },
+
+  // nested
+  {
+    path: 'products',
+    component: ProductsComponent,
+    children: childRoutes
+  }
+];
 
 @NgModule({
   declarations: [
