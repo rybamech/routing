b84c37a3c688549a5bd468a68e4d03990f28d278
DimaAdmin <ryba-mech@yandex.ru>
2018-05-16 09:18:08 +0300

    02- add in app.component.html directive to link and tag router-outlet. Edit app.component.ts -> constructor(private router: Router)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {
+  Router
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private router: Router) {
+  };
 }
+
